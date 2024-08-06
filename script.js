8024f2e52a43fa7d354ee427ab1c0f88b2ebeb72
Abhishek Isar <156158149+Abhishekaaa123@users.noreply.github.com>
2024-08-07 00:43:43 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Registration successful!');
+    // Add additional form handling logic here
+});
